116259a23127c5b97f5e74cbf8e2ee79fbcea08a
Lee Baesler <lee.baesler@gmail.com>
2022-09-02 23:30:22 -0500

    apend function and variable creation

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,4 +4,20 @@ function onReady(){
     console.log("JQ and JS");
 
 }
+function addEmployee ();{
+const employeeFirstName =$("#firstName").val();
+const employeeLasttName =$("#lastName").val();
+const employeeID =$("#id").val();
+const employeeTitle =$("#title").val();
+const employeeAnnualSalary=$("#annualSalary").val();
 
+$("#tableBody").append
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td><button id="deleteButton"> Delete</button></td>
+    </tr>
+
+}
