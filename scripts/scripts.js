37cac984578ea05216d3097c009ff42be54ed61c
Lee Baesler <lee.baesler@gmail.com>
2022-09-03 01:39:43 -0500

    appending captured info is not going to correct spot on dom. getting uploaded above td tags and not below

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,21 +3,27 @@ $(onReady);
 function onReady(){
     console.log("JQ and JS");
 
+    clickHandlers ();
 }
-function addEmployee ();{
+
+function clickHandlers (){
+    $("#addButton").on("click", addEmployee)
+}
+function addEmployee (){
 const employeeFirstName =$("#firstName").val();
 const employeeLasttName =$("#lastName").val();
 const employeeID =$("#id").val();
 const employeeTitle =$("#title").val();
 const employeeAnnualSalary=$("#annualSalary").val();
 
-$("#tableBody").append
+$("#tableBody").append(`
     <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
+        <td>${employeeFirstName}</td>
+        <td>${employeeLasttName}</td>
+        <td>${employeeID}</td>
+        <td>${employeeTitle}</td>
+        <td>${employeeAnnualSalary}</td>
         <td><button id="deleteButton"> Delete</button></td>
     </tr>
-
+`)
 }
