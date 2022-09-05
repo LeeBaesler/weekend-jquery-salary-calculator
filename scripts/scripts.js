7fa3d76cb568a6c0ced4f6e381fa222176b9a682
Lee Baesler <lee.baesler@gmail.com>
2022-09-04 22:34:26 -0500

    code is working until the appending of captured values from annual salary to total monthly. will continue to working

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,11 @@
 $(onReady);
 
-let annualSalary = [];
+
 
 function onReady(){
     console.log("JQ and JS");
+
+    const totalSalary = [];
 
     clickHandlers ();
 }
@@ -13,7 +15,7 @@ function addEmployee (){
     let employeeID =$("#id").val();
     let employeeTitle =$("#title").val();
     let employeeAnnualSalary=$("#annualSalary").val();
-    
+
 console.log(employeeFirstName, employeeLastName,employeeID,employeeTitle, employeeAnnualSalary)
 
     $("#tableBody").append(`
@@ -24,31 +26,21 @@ console.log(employeeFirstName, employeeLastName,employeeID,employeeTitle, employ
             <td>${employeeTitle}</td>
             <td>${employeeAnnualSalary}</td>
             <td><button class="deleteButton"> Delete</button></td>
-        </tr>
+            <br/>
+            <br/>
+            <br/>
+            </tr>
     `);
+
     $("#firstName").val("");
     $("#lastName").val("");
     $("#id").val("");
     $("#title").val("");
     $("#annualSalary").val("");
 
-    $('#annualSalary').append(
-        `<h3>Add multiple:</h3>
-        <input id="annualSalary" placeholder="Quantity"/>
-        <input id="colorIn" placeholder="Color"/>
-        <button id="addMultiple">Add Multiple</button>
-        <br/>
-        <br/>`)
-
-        $('#addMultiple').on('click', ()=>{
-            let annualSalary = $('#quantityIn').val();
-            let colorIn = $('#colorIn').val();
-            $('#quantityIn').val('');
-            $('#colorIn').val('');
-            addMultiple(quantIn, colorIn);
-        })
-
+    
     }
+    
     
 
 function deleteEmployee (event){
@@ -61,4 +53,8 @@ function clickHandlers (){
 
     $("#tableBody").on("click", ".deleteButton", deleteEmployee)
 
+}
+
+function formSubmitHandler(){
+    let annualSalary = $("#totalSalary").val(); 
 }
