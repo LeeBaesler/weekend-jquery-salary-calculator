7396c733572ef0b118a349c43e3f1e4afaf6ee31
Lee Baesler <lee.baesler@gmail.com>
2022-09-05 19:01:00 -0500

    created function that captures annual salary and shows monthly total. need to work with if statement and css to show red if over ,000

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,25 +1,25 @@
-$(onReady);
 
-function onReady(){
+$(document).ready(function () {
     
     console.log("JQ and JS");
 
-    clickHandlers ();
+    updateMonthlyTotal();
 
-    
+    $("#addButton").on("click", addEmployee);
 
-}
-let totalMonthlySalary = [];
+    $("#tableBody").on("click", ".deleteButton", deleteEmployee);
 
-function addEmployee (){
+});
+let totalMonthlySalary = 0;
+
+function addEmployee (event){
+    $('#no-employees').remove();
+
     let employeeFirstName =$("#firstName").val();
     let employeeLastName =$("#lastName").val();
     let employeeID =Number($("#id").val());
     let employeeTitle =$("#title").val();
-    let employeeAnnualSalary=$("#annualSalary").val();
-    let workingSalary = totalMonthlySalary *12;
-
-    totalMonthlySalary.($("#totalMonthlySalary").text(`${totalMonthlySalary}`));
+    let employeeAnnualSalary= Number($("#annualSalary").val());
     
 
     console.log(employeeFirstName, employeeLastName,employeeID,employeeTitle, employeeAnnualSalary)
@@ -32,40 +32,29 @@ function addEmployee (){
             <td>${employeeTitle}</td>
             <td>${employeeAnnualSalary}</td>
             <td><button class="deleteButton"> Delete</button></td>
-            </tr>
+            <br/>
+            <br/>
+            <br/>
+         </tr>   
     `);
+
+    totalMonthlySalary += employeeAnnualSalary;
+    updateMonthlyTotal();
 
     $("#firstName").val("");
     $("#lastName").val("");
     $("#id").val("");
     $("#title").val("");
     $("#annualSalary").val("");
-
-const completeSalary = {
-
     
 }
 
-totalMonthlySalary.push(completeSalary)
-console.log(`there are now ${totalMonthlySalary.length} annual`);
+   function updateMonthlyTotal() {
+    let monthlyTotal = totalMonthlySalary /12;
+    $("#totalMonthlySalary").text(monthlyTotal.toFixed(2));
 
-for (let i=0; i<totalMonthlySalary.length; i++) {
-    console.log(`showing`, totalMonthlySalary[i]);
-    $("#totalSalary").append(`${totalMonthlySalary[i].employeeAnnualSalary}`)
-}
-
-}
-
+   }
 
 function deleteEmployee (event){
     $(event.target).closest('tr').remove()
 }
-
-function clickHandlers (){
-   
-    $("#addButton").on("click", addEmployee)
-
-    $("#tableBody").on("click", ".deleteButton", deleteEmployee)
-
-}
-
